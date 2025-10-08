dbe64d28598815534786875ea2f36572370cc474
nourelafifi <nourelafifiworks@gmail.com>
2025-10-08 19:45:47 +0300

    correcting hover

--- src/components/ServicesSection/ServicesCard.jsx
+++ src/components/ServicesSection/ServicesCard.jsx
@@ -4,7 +4,7 @@ export default function SCard ({icon,title,hoverImage,description}) {
     const [ hovered , setHovered ] = useState(false)
     const handleToggle = () => setHovered(!hovered);
     return (
-        <div className= {styles.sCard} style={{ "--hover-img": `url(${hoverImage})` }} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} onClick={handleToggle}>
+        <div className= {styles.sCard} style={{ "--hover-img": `url(${hoverImage})` }}onClick={handleToggle}>
             {!hovered && (
             <>
                 <div className= {styles.iconCircle}>{icon}</div>
