ef1dac0d3e045b3f68de15c7564401f357834009
nourelafifi <nourelafifiworks@gmail.com>
2025-10-08 19:54:39 +0300

    undoing hover

--- src/components/ServicesSection/ServicesCard.jsx
+++ src/components/ServicesSection/ServicesCard.jsx
@@ -2,9 +2,8 @@ import styles from './services.module.css'
 import { useState } from 'react'
 export default function SCard ({icon,title,hoverImage,description}) {
     const [ hovered , setHovered ] = useState(false)
-    const handleToggle = () => setHovered(!hovered);
     return (
-        <div className= {styles.sCard} style={{ "--hover-img": `url(${hoverImage})` }}onClick={handleToggle}>
+        <div className= {styles.sCard} style={{ "--hover-img": `url(${hoverImage})` }}onMouseEnter={setHovered(true)} onMouseLeave={setHovered(false)}>
             {!hovered && (
             <>
                 <div className= {styles.iconCircle}>{icon}</div>
