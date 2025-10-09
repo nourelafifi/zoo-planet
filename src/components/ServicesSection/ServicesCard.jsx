783699cd9d05d177ceb0a5645acb8e3dc23018cc
nourelafifi <nourelafifiworks@gmail.com>
2025-10-09 09:18:32 +0300

    fixing hovering

--- src/components/ServicesSection/ServicesCard.jsx
+++ src/components/ServicesSection/ServicesCard.jsx
@@ -2,9 +2,16 @@ import styles from './services.module.css'
 import { useState } from 'react'
 export default function SCard ({icon,title,hoverImage,description}) {
     const [ hovered , setHovered ] = useState(false)
-    // onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}
-    return (
-        <div className= {styles.sCard} style={{ "--hover-img": `url(${hoverImage})` }}>
+    const hovering = hovered? {
+        transform: 'rotate(360deg)', 
+        backgroundImage: `url(${hoverImage})`,
+        borderRadius: '10%'}:
+        {
+        backgroundImage: 'linear-gradient(to top right, #93a55d, #afc56b)',
+        borderRadius: '50%'
+        };
+    return(
+        <div className= {styles.sCard} style={hovering} onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}>
             {!hovered && (
             <>
                 <div className= {styles.iconCircle}>{icon}</div>
