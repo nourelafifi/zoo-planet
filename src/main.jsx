5e0a7946aff368bd74d673c1470ee03ff6f9f544
nourelafifi <nourelafifiworks@gmail.com>
2026-01-29 22:25:40 +0200

    debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,5 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";  // Import HashRouter
-
-createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <HashRouter>   {/* Wrap the app in HashRouter for GitHub Pages */}
-            <App />
-        </HashRouter>
-    </React.StrictMode>
-);
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+createRoot(document.getElementById('root')).render(
+    <App />
+)
